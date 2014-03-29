40f0b1b3b40c45251864c913afff62a90bde0ea9
jc <emclab2011@gmail.com>
2014-03-28 21:49:47 -0500

    JC: added datepicker for out date.

--- app/assets/javascripts/item_checkoutx/item_checkouts.js
+++ app/assets/javascripts/item_checkoutx/item_checkouts.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+	$( "#checkout_out_date" ).datepicker({dateFormat: 'yy-mm-dd'});
+	
+});
